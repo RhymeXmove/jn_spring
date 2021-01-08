3680216499e0f2e47c0bc2a93b8ba59889656bb6
RhymeXmove <61677430+RhymeXmove@users.noreply.github.com>
2021-01-08 22:42:10 +0800

    2021/01/08/22:42

--- src/main/java/org/main/UserTest.java
+++ src/main/java/org/main/UserTest.java
@@ -9,11 +9,20 @@ public class UserTest {
         ClassPathXmlApplicationContext ctx =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        User user1 = (User) ctx.getBean("user");
-        System.out.println(JSON.toJSONString(user1));
+        User user_object = (User) ctx.getBean("user_object");
+        System.out.println(JSON.toJSONString(user_object));
 
-        User user2 = (User) ctx.getBean("user2");
-        System.out.println(JSON.toJSONString(user2));
+        User user_arry = (User) ctx.getBean("user_arry");
+        System.out.println(JSON.toJSONString(user_arry));
+
+        User user_list = (User) ctx.getBean("user_list");
+        System.out.println(JSON.toJSONString(user_list));
+
+        User user_map = (User) ctx.getBean("user_map");
+        System.out.println(JSON.toJSONString(user_map));
+
+        User user_properties = (User) ctx.getBean("user_properties");
+        System.out.println(JSON.toJSONString(user_properties));
 
     }
 }
