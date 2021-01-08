bc839cd443ddc6048856bfe3e6aff070a1eb9646
RhymeXmove <61677430+RhymeXmove@users.noreply.github.com>
2021-01-08 19:09:36 +0800

    提交0.0.1

---
+++ src/main/java/org/javaboy/Book.java
@@ -0,0 +1,43 @@
+package org.javaboy;
+
+public class Book {
+    private Integer id;
+    private String name;
+    private Double price;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+
+    public Book() {
+        System.out.println("----------------book init-----------------------");
+    }
+
+    public Book(Integer id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price =price;
+
+    }
+}
