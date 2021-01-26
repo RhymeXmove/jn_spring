48ecafc2df5e85f16cd09fa071cfb071652a4dcf
Lenovo <1766984932@qq.com>
2021-01-26 14:58:30 +0800

    事务 注解@transactional 2021/01/26/14:58

--- src/main/java/org/aop/component/LogAspectTest.java
+++ src/main/java/org/aop/component/LogAspectTest.java
@@ -1,5 +1,7 @@
 package org.aop.component;
+import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
 
 public class LogAspectTest {
     public static void main(String[] args) {
@@ -9,4 +11,15 @@ public class LogAspectTest {
         myCalculator.add(2,4);
         myCalculator.min(2,4);
     }
+
+    @Test
+    @Transactional
+    public void transactional() {
+        AnnotationConfigApplicationContext ctx
+                = new AnnotationConfigApplicationContext(JavaConfig.class);
+        MyCalculatorImpl myCalculator = ctx.getBean(MyCalculatorImpl.class);
+        myCalculator.add(2,4);
+        myCalculator.min(-1,4);
+    }
+
 }
